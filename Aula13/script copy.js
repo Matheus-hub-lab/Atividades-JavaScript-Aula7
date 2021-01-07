d5b0898baed3a7852ed501d46f8e7f804366bed6
Matheus Viacava Sigoli <75397672+Matheus-hub-lab@users.noreply.github.com>
2021-01-07 09:40:10 -0300

    Add files via upload

---
+++ Aula13/script copy.js
@@ -0,0 +1,17 @@
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if(num.nodeValue.length == 0){
+        window.alert('Por favor, digite um numero!')
+    }else{
+        let n = Number(num.value)
+        let c = 1
+        while(c <= 10){
+            let item = document.createElement('option')
+            item.text=`${n} x ${c} = ${num*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
